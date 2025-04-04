0658b7011c47230213de4f521920b16b95871316
Theodore Lodholz <tlodholz@gmail.com>
2025-04-04 08:35:23 -0400

    Created framework to components to add and edit customer

---
+++ MyBlazorApp/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyBlazorApp.Models
+{
+    public class Customer
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)] // Example: Maximum length of 100 characters
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100)] // Example: Maximum length of 100 characters
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress] // Ensures valid email format
+        public string Email { get; set; }
+
+        [Required]
+        [Phone] // Ensures valid phone number format
+        public string PhoneNumber { get; set; }
+
+    }
+}
